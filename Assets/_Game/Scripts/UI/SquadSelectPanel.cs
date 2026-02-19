6d2fbabdccd9c828d2f52545da1682d2c541fb79
Vladislav Ryazantsev <90804222+zZDraculaZz@users.noreply.github.com>
2026-02-19 14:36:08 +0300

    Move HUD panels to overlay layer outside map viewport

---
+++ Assets/_Game/Scripts/UI/SquadSelectPanel.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using FantasyGuildmaster.Map;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FantasyGuildmaster.UI
+{
+    public sealed class SquadSelectPanel : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text titleText;
+        [SerializeField] private RectTransform listRoot;
+        [SerializeField] private Button squadButtonPrefab;
+        [SerializeField] private Button closeButton;
+
+        private readonly List<Button> _buttons = new();
+
+        public void Show(List<SquadData> idleSquads, Action<SquadData> onSelected)
+        {
+            gameObject.SetActive(true);
+
+            if (titleText != null)
+            {
+                titleText.text = "Assign Squad";
+            }
+
+            ClearButtons();
+
+            for (var i = 0; i < idleSquads.Count; i++)
+            {
+                var squad = idleSquads[i];
+                var button = Instantiate(squadButtonPrefab, listRoot);
+                button.gameObject.SetActive(true);
+                var label = button.GetComponentInChildren<TMP_Text>();
+                if (label != null)
+                {
+                    label.text = $"{squad.name} ({squad.membersCount})";
+                }
+
+                button.onClick.RemoveAllListeners();
+                button.onClick.AddListener(() =>
+                {
+                    onSelected?.Invoke(squad);
+                    Hide();
+                });
+
+                _buttons.Add(button);
+            }
+
+            if (closeButton != null)
+            {
+                closeButton.onClick.RemoveAllListeners();
+                closeButton.onClick.AddListener(Hide);
+            }
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void ClearButtons()
+        {
+            for (var i = 0; i < _buttons.Count; i++)
+            {
+                if (_buttons[i] != null)
+                {
+                    Destroy(_buttons[i].gameObject);
+                }
+            }
+
+            _buttons.Clear();
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/UI/SquadStatusRow.cs
@@ -0,0 +1,83 @@
+using TMPro;
+using UnityEngine;
+
+namespace FantasyGuildmaster.UI
+{
+    public sealed class SquadStatusRow : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text squadNameText;
+        [SerializeField] private TMP_Text statusTimerText;
+        [SerializeField] private TMP_Text hpText;
+
+        private bool _loggedBind;
+
+        public void ConfigureRuntime(TMP_Text squadName, TMP_Text statusTimer, TMP_Text hp)
+        {
+            squadNameText = squadName;
+            statusTimerText = statusTimer;
+            hpText = hp;
+        }
+
+        public void SetData(string squadName, string statusTimer, string hp, Color statusColor)
+        {
+            EnsureReferences();
+
+            if (squadNameText != null)
+            {
+                squadNameText.text = squadName;
+            }
+
+            if (statusTimerText != null)
+            {
+                statusTimerText.text = statusTimer;
+                statusTimerText.color = statusColor;
+            }
+
+            if (hpText != null)
+            {
+                hpText.text = hp;
+                hpText.color = statusColor;
+            }
+
+            if (!_loggedBind)
+            {
+                Debug.Log($"[HUDDebug] Row bind ok: {squadName}");
+                _loggedBind = true;
+            }
+        }
+
+        private void EnsureReferences()
+        {
+            if (squadNameText == null)
+            {
+                squadNameText = FindText("NameText") ?? FindText("SquadName");
+            }
+
+            if (statusTimerText == null)
+            {
+                statusTimerText = FindText("StatusText") ?? FindText("StatusTimer");
+            }
+
+            if (hpText == null)
+            {
+                hpText = FindText("HpText") ?? FindText("Hp");
+            }
+        }
+
+        private TMP_Text FindText(string name)
+        {
+            var transforms = GetComponentsInChildren<Transform>(true);
+            for (var i = 0; i < transforms.Length; i++)
+            {
+                if (transforms[i].name != name)
+                {
+                    continue;
+                }
+
+                return transforms[i].GetComponent<TMP_Text>();
+            }
+
+            return null;
+        }
+    }
+}
